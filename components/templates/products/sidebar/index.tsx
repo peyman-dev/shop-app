ff8c794e9807447138647a22d5f4a8d057d45bcb
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-02 14:22:35 +0330

    Initialized the Sidebar/[Filters]

--- components/templates/products/sidebar/index.tsx
+++ components/templates/products/sidebar/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import PromotedProduct from './components/promoted-product/'
+import Filters from './components/filters'
 
 const Sidebar = () => {
   return (
-    <aside className='w-[270px]! *:border *:p-4 *:border-neutral-200 *:rounded-lg'>
+    <aside className='w-[270px]! *:border *:p-4 space-y-5 *:border-neutral-200 *:rounded-lg'>
         <PromotedProduct />
+        <Filters />
     </aside>
   )
 }
