c5e6024f2d0c82d02f7037da4b5abe31504d1c40
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-02 14:13:06 +0330

    Added 'Toman' Icon

---
+++ components/icons/toman.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import TomanIcon from "@/assets/static/toman.svg";
+import Image from "next/image";
+
+const Toman = () => {
+  return <Image src={TomanIcon} width={28} height={28} alt="تومان" />;
+};
+
+export default Toman;
