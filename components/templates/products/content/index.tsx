c8cf24c2c9034650fa98c5a58f7a15cc3e115704
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-04 09:18:13 +0330

    Added static List of Products

--- components/templates/products/content/index.tsx
+++ components/templates/products/content/index.tsx
@@ -1,8 +1,14 @@
+import ProductCard from '@/components/common/cards/product'
+import { arrayMap } from '@/core/features/array-map'
 import React from 'react'
 
 const Content = () => {
   return (
-    <div>Content</div>
+    <section className='grid grow w-full grid-cols-4 divide-x divide-y'>
+      {
+        arrayMap(50, <ProductCard />)
+      }
+    </section>
   )
 }
 
