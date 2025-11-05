7a5f091d4f9b917848191533e1914555af2a3529
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-05 14:13:57 +0330

    Sort By Component Initialized & Added to /products page

--- components/templates/products/content/index.tsx
+++ components/templates/products/content/index.tsx
@@ -1,15 +1,17 @@
-import ProductCard from '@/components/common/cards/product'
-import { arrayMap } from '@/core/features/array-map'
-import React from 'react'
+import ProductCard from "@/components/common/cards/product";
+import { arrayMap } from "@/core/features/array-map";
+import React from "react";
+import SortBy from "./sort-by";
 
 const Content = () => {
   return (
-    <section className='grid grow w-full grid-cols-4 divide-x divide-y'>
-      {
-        arrayMap(50, <ProductCard />)
-      }
+    <section>
+      <SortBy />
+      <div className="grid grow w-full grid-cols-4 divide-x divide-y">
+        {arrayMap(50, <ProductCard />)}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
