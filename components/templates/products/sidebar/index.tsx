68d7e4ac6c4f848f91063b7b79fc93f91dfa736d
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-05 14:14:09 +0330

    Fixed Sidebar size

--- components/templates/products/sidebar/index.tsx
+++ components/templates/products/sidebar/index.tsx
@@ -4,7 +4,7 @@ import Filters from './components/filters'
 
 const Sidebar = () => {
   return (
-    <aside className='w-[270px]! *:border *:p-4 space-y-5 *:border-neutral-200 *:rounded-lg'>
+    <aside className='w-[270px]! min-w-[270px] *:border *:p-4 space-y-5 *:border-neutral-200 *:rounded-lg'>
         <PromotedProduct />
         <Filters />
     </aside>
