a05adb9cda43794bdf264514defb2973a65b8136
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-08 12:54:54 +0330

    Added Redirect module to header.AuthButton

--- components/common/header/components/desktop/top-bar/profile-dropdown/index.tsx
+++ components/common/header/components/desktop/top-bar/profile-dropdown/index.tsx
@@ -1,11 +1,14 @@
+"use client"
 import { Button } from "@/components/ui/button";
 import { LogIn } from "lucide-react";
+import { redirect, RedirectType } from "next/navigation";
 import React from "react";
 
 const ProfileDropdown = () => {
+  const redirector = () => redirect('/auth/login?redirect=/', RedirectType.push)
   return (
     <div>
-      <Button variant="outline" color="white" className="h-10 px-[17px] text-xs border border-[#E0E0E2] rounded-lg cursor-pointer duration-150 hover:bg-neutral-50 flex-center gap-2">
+      <Button onClick={redirector} variant="outline" color="white" className="h-10 px-[17px] text-xs border border-[#E0E0E2] rounded-lg cursor-pointer duration-150 hover:bg-neutral-50 flex-center gap-2">
         <LogIn className="size-4" />
         <span>ورود | ثبت نام</span>
       </Button>
