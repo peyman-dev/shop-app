240c9a10aca7eb6cadf1900e0d06de5de1199b0c
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-08 14:20:05 +0330

    AuthLayout got Dynamic AuthHeader

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,10 +1,12 @@
 "use client"
+import AuthHeader from "@/components/templates/auth/common/auth-header";
 import React, { ReactNode } from "react";
 
 const layout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="w-dvw bg-[#ffffff] h-[calc(100dvh-140px)] flex-center">
-      <form action="#" className="bg-white w-[420px] rounded-md shadow border border-neutral-200 p-4" onSubmit={(e) => e.preventDefault()}>
+      <form action="#" className="bg-white space-y-5 w-[420px] rounded-md shadow border border-neutral-200 p-4" onSubmit={(e) => e.preventDefault()}>
+        <AuthHeader />
         {children}
       </form>
     </main>
