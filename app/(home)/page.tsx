1d5eed95a9e6139a764e0408cfadc6509c569749
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-11 13:59:31 +0330

    Remove *:container from Home Elements

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -11,7 +11,7 @@ import React from "react";
 
 const HomePage = () => {
   return (
-    <main id="home-pag-content" className="*:my-10 *:first:my-0! container">
+    <main id="home-pag-content" className="*:my-10 *:first:my-0!">
       <Stories />
       <HeroSlider />
       <LatestProducts />
