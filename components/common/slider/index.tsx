ded4fca01025aaf2e396154bb73eba26361c3d9c
Peyman Ahmadi <peymangeek@gmail.com>
2025-11-11 09:56:12 +0330

    Modified the Slider

--- components/common/slider/index.tsx
+++ components/common/slider/index.tsx
@@ -5,7 +5,14 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import type { SwiperProps, SwiperRef } from "swiper/react";
 import "swiper/css";
 
-import { Navigation, Pagination, Scrollbar, A11y } from "swiper/modules";
+import {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+  FreeMode,
+} from "swiper/modules";
 import clsx from "clsx";
 
 type SliderProps = {
@@ -13,7 +20,7 @@ type SliderProps = {
 } & Omit<SwiperProps, "children">;
 
 export const useSlider = () => {
-  const swiperRef = React.useRef<SwiperRef | null>(null);
+  const swiperRef = React.useRef<SwiperRef>(null);
 
   const slideNext = () => {
     if (swiperRef.current) {
@@ -27,9 +34,17 @@ export const useSlider = () => {
     }
   };
 
-  const ButtonsWrapper = ({ children, className }: { children: React.ReactNode, className?: string }) => {
+  const ButtonsWrapper = ({
+    children,
+    className,
+  }: {
+    children: React.ReactNode;
+    className?: string;
+  }) => {
     return (
-      <div className={clsx("absolute bottom-4 z-10000!  flex gap-3", className)}>
+      <div
+        className={clsx("absolute bottom-4 z-10000!  flex gap-3", className)}
+      >
         {children}
       </div>
     );
@@ -41,13 +56,15 @@ export const useSlider = () => {
 const Slider = forwardRef<SwiperRef, SliderProps>(
   ({ children, ...props }, ref) => {
     return (
-      <div className="relative max-h-max">
-        <Swiper loop ref={ref} {...props} modules={[Navigation, Pagination, Scrollbar, A11y]}>
-          {Children.map(children, (child, i) => (
-            <SwiperSlide key={i}>{child}</SwiperSlide>
-          ))}
-        </Swiper>
-      </div>
+      <Swiper
+        ref={ref}
+        {...props}
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay, FreeMode]}
+      >
+        {Children.map(children, (child, i) => (
+          <SwiperSlide key={i}>{child}</SwiperSlide>
+        ))}
+      </Swiper>
     );
   }
 );
